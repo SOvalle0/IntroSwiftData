cb9ae0a3de6345b0c727a2682084dc69606240b6
Sebastian O <sovalle2000@gmail.com>
2023-06-13 22:26:40 -0400

    Initial Commit

---
+++ IntroSwiftData/IntroSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSwiftDataApp.swift
+//  IntroSwiftData
+//
+//  Created by Macbook Air on 13-06-23.
+//
+
+import SwiftUI
+
+@main
+struct IntroSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
